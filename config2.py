311727eb4559cbc9bb1df95d2b0538353adbb9c5
akansha-nec <91887826+akansha-nec@users.noreply.github.com>
2021-11-01 10:06:24 +0530

    Add files via upload

---
+++ config2.py
@@ -0,0 +1,14 @@
+    client id =  A3T6AKIAFJKR45SAWS5Z
+    client id = 932934478677
+    SECRET = df024bd34fe06869b1d7f887da00bd20ca3b705c7867efe0f2c0f6d644c8d9eb
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+    
+ 
+    
+    client id =  A3T6AKIAFJKR1245SAWS
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+    
+    client id =  DUMMY_A3T6AKIAFJKDUMMYSAWS
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+
+https://ap-south-1.console.aws.amazon.com/console/home?region=ap-south-1#
